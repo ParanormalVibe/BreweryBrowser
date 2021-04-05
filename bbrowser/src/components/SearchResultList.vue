58f30509cbfff8df97b0cec97239884f625ab364
Paranormal Vibe <jaray@everygame.co>
2021-04-05 14:22:11 -0400

    Add placeholder SearchResultList content

--- bbrowser/src/components/SearchResultList.vue
+++ bbrowser/src/components/SearchResultList.vue
@@ -11,12 +11,33 @@ export default {
   components: {SearchResult},
   computed: {
     results() {
-      return [] // placeholder for vuex store object
+      return [
+        {
+          id: 5494,
+          name: "MadTree Brewing",
+          brewery_type: "regional",
+          street: "3301 Madison Rd",
+          address_2: null,
+          address_3: null,
+          city: "Cincinnati",
+          county_province: null,
+          state: "Ohio",
+          postal_code: "45209-1132",
+          country: "United States",
+          phone: "5138368733",
+          website_url: "http://www.madtreebrewing.com"
+        }
+      ] // placeholder for vuex store object
     }
   }
 }
 </script>
 
 <style scoped>
-
+.search-result-list {
+  width: 600px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+}
 </style>
