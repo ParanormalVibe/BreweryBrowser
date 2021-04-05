ec369b560596e5c04607be0dc44fd0d04972ac58
Paranormal Vibe <jaray@everygame.co>
2021-04-05 13:55:24 -0400

    Add SearchResultList logic

--- bbrowser/src/components/SearchResultList.vue
+++ bbrowser/src/components/SearchResultList.vue
@@ -1,10 +1,19 @@
 <template>
-
+  <div class="search-result-list">
+    <SearchResult v-for="result in results" :key="result.id" :brewery="result" />
+  </div>
 </template>
 
 <script>
+import SearchResult from "./SearchResult";
 export default {
-name: "SearchResultList"
+  name: "SearchResultList",
+  components: {SearchResult},
+  computed: {
+    results() {
+      return [] // placeholder for vuex store object
+    }
+  }
 }
 </script>
 
