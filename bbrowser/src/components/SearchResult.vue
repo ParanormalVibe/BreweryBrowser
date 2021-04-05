73e9ef73617f5d92dd5e5ee1539979bf37771233
Paranormal Vibe <jaray@everygame.co>
2021-04-05 13:43:40 -0400

    Draft search result model

--- bbrowser/src/components/SearchResult.vue
+++ bbrowser/src/components/SearchResult.vue
@@ -1,5 +1,19 @@
 <template>
-  <div></div>
+  <div class="search-result row">
+    <div class="col">
+      <span>{{ breweryName }}</span>
+      <span>{{ breweryPhone }}</span>
+      <span>{{ breweryAddress }}</span>
+      <span v-if="breweryAddress2">{{ breweryAddress2 }}</span>
+      <span v-if="breweryAddress3">{{ breweryAddress3 }}</span>
+      <span>{{ breweryCity }}, {{ breweryState }} {{ breweryPostalCode }}</span>
+      <span v-if="breweryProvince">{{ breweryProvince }}</span>
+      <span>{{ breweryCountry }}</span>
+    </div>
+    <div class="col">
+      <span>{{ breweryType }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +23,7 @@ export default {
 </script>
 
 <style scoped>
-
+.search-result {
+  width: 600px;
+}
 </style>
