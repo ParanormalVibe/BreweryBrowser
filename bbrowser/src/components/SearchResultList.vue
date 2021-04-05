6448b4d3de4918c04af4c7f9cd264ba70c14279e
Paranormal Vibe <jaray@everygame.co>
2021-04-05 14:33:47 -0400

    Change SearchResultList to pull from store

--- bbrowser/src/components/SearchResultList.vue
+++ bbrowser/src/components/SearchResultList.vue
@@ -11,23 +11,7 @@ export default {
   components: {SearchResult},
   computed: {
     results() {
-      return [
-        {
-          id: 5494,
-          name: "MadTree Brewing",
-          brewery_type: "regional",
-          street: "3301 Madison Rd",
-          address_2: null,
-          address_3: null,
-          city: "Cincinnati",
-          county_province: null,
-          state: "Ohio",
-          postal_code: "45209-1132",
-          country: "United States",
-          phone: "5138368733",
-          website_url: "http://www.madtreebrewing.com"
-        }
-      ] // placeholder for vuex store object
+      return this.$store.state.searchResults
     }
   }
 }
