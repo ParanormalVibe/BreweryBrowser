031390bd1acdcc1614e58e848b8d89ca72e51e52
Paranormal Vibe <jaray@everygame.co>
2021-04-05 14:32:34 -0400

    Add vuex store

--- bbrowser/src/main.js
+++ bbrowser/src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'es6-promise/auto'
 
-createApp(App).mount('#app')
+const store = createStore({
+    state() {
+        return {
+            searchResults: []
+        }
+    }
+})
+
+const app = createApp(App)
+app.use(store)
+app.mount('#app')
