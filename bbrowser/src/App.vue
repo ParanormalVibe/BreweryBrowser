8817e46415f9cbbd9e16c14aa712dba1338d5753
Paranormal Vibe <jaray@everygame.co>
2021-04-05 12:31:05 -0400

    Restructure App.vue

--- bbrowser/src/App.vue
+++ bbrowser/src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app-container">
+    <Banner />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Banner from './components/Banner'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Banner
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app-container {
+  
 }
 </style>
