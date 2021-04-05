7c2a699c4dd2241b383a0deb5bce3d991c1aac30
Paranormal Vibe <jaray@everygame.co>
2021-04-05 14:20:21 -0400

    Fix brewery prop names

--- bbrowser/src/components/SearchResult.vue
+++ bbrowser/src/components/SearchResult.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="search-result row">
-    <div class="col">
+    <div class="col left-details">
       <span>{{ brewery.name }}</span>
       <span>{{ brewery.phone }}</span>
       <span>{{ brewery.address }}</span>
-      <span v-if="brewery.address2">{{ brewery.address2 }}</span>
-      <span v-if="brewery.address3">{{ brewery.address3 }}</span>
-      <span>{{ brewery.city }}, {{ brewery.state }} {{ brewery.postalCode }}</span>
-      <span v-if="brewery.province">{{ brewery.province }}</span>
+      <span v-if="brewery.address_2">{{ brewery.address_2 }}</span>
+      <span v-if="brewery.address_3">{{ brewery.address_3 }}</span>
+      <span>{{ brewery.city }}, {{ brewery.state }} {{ brewery.postal_code }}</span>
+      <span v-if="brewery.county_province">{{ brewery.county_province }}</span>
       <span>{{ brewery.country }}</span>
     </div>
-    <div class="col">
-      <span>{{ brewery.type }}</span>
+    <div class="col right-details">
+      <span>{{ brewery.brewery_type }}</span>
     </div>
   </div>
 </template>
