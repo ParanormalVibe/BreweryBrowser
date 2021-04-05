29f96f9f570765bb29bbd529c225a4f76266cb38
Paranormal Vibe <jaray@everygame.co>
2021-04-05 13:50:56 -0400

    Fix prop capitalization

--- bbrowser/src/components/SearchResult.vue
+++ bbrowser/src/components/SearchResult.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="search-result row">
     <div class="col">
-      <span>{{ brewery.Name }}</span>
-      <span>{{ brewery.Phone }}</span>
-      <span>{{ brewery.Address }}</span>
-      <span v-if="brewery.Address2">{{ brewery.Address2 }}</span>
-      <span v-if="brewery.Address3">{{ brewery.Address3 }}</span>
-      <span>{{ brewery.City }}, {{ brewery.State }} {{ brewery.PostalCode }}</span>
-      <span v-if="brewery.Province">{{ brewery.Province }}</span>
-      <span>{{ brewery.Country }}</span>
+      <span>{{ brewery.name }}</span>
+      <span>{{ brewery.phone }}</span>
+      <span>{{ brewery.address }}</span>
+      <span v-if="brewery.address2">{{ brewery.address2 }}</span>
+      <span v-if="brewery.address3">{{ brewery.address3 }}</span>
+      <span>{{ brewery.city }}, {{ brewery.state }} {{ brewery.postalCode }}</span>
+      <span v-if="brewery.province">{{ brewery.province }}</span>
+      <span>{{ brewery.country }}</span>
     </div>
     <div class="col">
-      <span>{{ brewery.Type }}</span>
+      <span>{{ brewery.type }}</span>
     </div>
   </div>
 </template>
