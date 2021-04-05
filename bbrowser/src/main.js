e404d282dcaeca554dc0c4a08924cee006bac1a1
Paranormal Vibe <jaray@everygame.co>
2021-04-05 15:20:31 -0400

    Add search query to state

--- bbrowser/src/main.js
+++ bbrowser/src/main.js
@@ -7,38 +7,13 @@ import 'es6-promise/auto'
 const store = createStore({
     state() {
         return {
-            searchResults: [
-                {
-                    id: 5494,
-                    name: "MadTree Brewing",
-                    brewery_type: "regional",
-                    street: "3301 Madison Rd",
-                    address_2: null,
-                    address_3: null,
-                    city: "Cincinnati",
-                    county_province: null,
-                    state: "Ohio",
-                    postal_code: "45209-1132",
-                    country: "United States",
-                    phone: "5138368733",
-                    website_url: "http://www.madtreebrewing.com"
-                },
-                {
-                    id: 5494,
-                    name: "MadTree Brewing",
-                    brewery_type: "regional",
-                    street: "3301 Madison Rd",
-                    address_2: null,
-                    address_3: null,
-                    city: "Cincinnati",
-                    county_province: null,
-                    state: "Ohio",
-                    postal_code: "45209-1132",
-                    country: "United States",
-                    phone: "5138368733",
-                    website_url: "http://www.madtreebrewing.com"
-                }
-            ]
+            searchResults: [],
+            searchQuery: ''
+        }
+    },
+    mutations: {
+        updateSearchQuery(state, query) {
+            state.searchQuery = query
         }
     }
 })
