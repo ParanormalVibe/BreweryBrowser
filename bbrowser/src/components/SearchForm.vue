23cd964ec713da235b25b09a24d23c4609a8d2aa
Paranormal Vibe <jaray@everygame.co>
2021-04-05 13:10:47 -0400

    Add search form style

--- bbrowser/src/components/SearchForm.vue
+++ bbrowser/src/components/SearchForm.vue
@@ -1,13 +1,25 @@
 <template>
-  <div></div>
+  <div class="search-form input-group mb-3">
+    <SearchInput />
+    <SearchButton />
+  </div>
 </template>
 
 <script>
+import SearchInput from "./SearchInput";
+import SearchButton from "./SearchButton";
 export default {
-name: "SearchForm"
+name: "SearchForm",
+  components: {SearchButton, SearchInput}
 }
 </script>
 
 <style scoped>
-
+.search-form {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 50%;
+  margin: 0 auto;
+}
 </style>
