b4a04c42c8f24548233fc2d2a44083a4fd29f25f
Paranormal Vibe <jaray@everygame.co>
2021-04-06 07:18:54 -0400

    Add paginator settings to vuex store

--- bbrowser/src/main.js
+++ bbrowser/src/main.js
@@ -3,12 +3,17 @@ import { createStore } from 'vuex'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'es6-promise/auto'
+import paginatorLogic from './paginatorLogic'
 
 const store = createStore({
     state() {
         return {
             searchResults: [],
-            searchQuery: ''
+            searchQuery: '',
+            paginatorSettings: {
+                itemsPerPage: 5,
+                pageIndex: 1
+            }
         }
     },
     mutations: {
@@ -17,6 +22,11 @@ const store = createStore({
         },
         updateSearchResults(state, searchResults) {
             state.searchResults = searchResults
+        }
+    },
+    getters: {
+        currentPage: (state) => {
+            return paginatorLogic.getPage(state.searchResults, state.paginatorSettings.pageIndex, state.paginatorSettings.itemsPerPage)
         }
     }
 })
