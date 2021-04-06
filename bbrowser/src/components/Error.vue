ffaf97778287b9d4179e5ec0cc83573521774498
Paranormal Vibe <jaray@everygame.co>
2021-04-06 08:52:17 -0400

    Add error component

---
+++ bbrowser/src/components/Error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app-error">
+    <h2>Uh oh!</h2>
+    <p>Looks like something went wrong! Please try again later.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error"
+}
+</script>
+
+<style scoped>
+.app-error {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
