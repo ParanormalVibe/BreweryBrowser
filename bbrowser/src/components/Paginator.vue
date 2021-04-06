f961499c916272dbc4e4b29b7a671027b4da72ef
Paranormal Vibe <jaray@everygame.co>
2021-04-06 08:51:41 -0400

    Complete paginator logic

--- bbrowser/src/components/Paginator.vue
+++ bbrowser/src/components/Paginator.vue
@@ -1,7 +1,13 @@
 <template>
-  <nav aria-label="Search result page navigation">
+  <nav aria-label="Search result page navigation" class="app-paginator">
     <ul class="pagination">
-
+      <li :class="{ disabled: (currentPage - 1 <= 0) }" class="page-item"><a @click="onPageClick(currentPage - 1)" class="page-link" href="#">Previous</a></li>
+      <li v-if="currentPage - 2 > 0" class="page-item"><a @click="onPageClick(currentPage - 2)" class="page-link" href="#">{{ currentPage - 2 }}</a></li>
+      <li v-if="currentPage - 1 > 0" class="page-item"><a @click="onPageClick(currentPage - 1)"  class="page-link" href="#">{{ currentPage - 1}}</a></li>
+      <li class="page-item active"><a class="page-link" href="#">{{ currentPage }}</a></li>
+      <li v-if="currentPage + 1 <= pageCount" class="page-item"><a @click="onPageClick(currentPage + 1)" class="page-link" href="#">{{ currentPage + 1}}</a></li>
+      <li v-if="currentPage + 2 <= pageCount" class="page-item"><a @click="onPageClick(currentPage + 2)" class="page-link" href="#">{{ currentPage + 2 }}</a></li>
+      <li v-if="currentPage + 1 <= pageCount" class="page-item"><a @click="onPageClick(currentPage + 1)" class="page-link" href="#">Next</a></li>
     </ul>
   </nav>
 </template>
@@ -9,10 +15,24 @@
 <script>
 export default {
   name: "Paginator",
-  emits: []
+  computed: {
+    pageCount() {
+      return this.$store.getters.currentPage.pages
+    },
+    currentPage() {
+      return this.$store.getters.currentPage.currentPage
+    }
+  },
+  methods: {
+    onPageClick(pageNumber) {
+      this.$store.commit('updatePageNumber', pageNumber)
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .app-paginator {
+    margin: 0 auto;
+  }
 </style>
